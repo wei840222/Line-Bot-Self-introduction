8101c9b3cde6041ba445d2f37ea3e7050b97f687
萬俊瑋 <wei840222@gmail.com>
2018-04-20 14:52:59 +0800

    add interface for button and test it

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+from interface import buttons
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('qIdHFZvo5+4GB4RnOmV+a+fIiSrAQJfXquzhZiNzAb/KzOkjQVvYXX9L9JTysNzDmfZe0pYkMWuwTiv4ocNsNeH9FRcyIS14ND5ZTgVOByI2Q/IAjgKYbI37a4tnX/KuIi+IEF5egYNn8lfLZehDCwdB04t89/1O/w1cDnyilFU=')
@@ -37,8 +39,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='hello wei!'))
+        event.reply_token, buttons)
 
 
 if __name__ == "__main__":
