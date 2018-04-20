a1eebaf4fd4015cd80d64f4187835f9b5b354c74
萬俊瑋 <wei840222@gmail.com>
2018-04-20 21:22:32 +0800

    try push

--- app.py
+++ app.py
@@ -33,6 +33,11 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(event.reply_token, msgIdentify(event.message.text))
+    try:
+    line_bot_api.push_message(event.source.userId, TextSendMessage(text='Hello World!'))
+        except LineBotApiError as e:
+            # error handle
+            pass
 
 
 if __name__ == "__main__":
