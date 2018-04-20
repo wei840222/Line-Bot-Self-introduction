86cd971c373ffb8f99e15e58dd2c971270bd229d
萬俊瑋 <wei840222@gmail.com>
2018-04-20 21:45:49 +0800

    print user profile on log

--- app.py
+++ app.py
@@ -33,6 +33,16 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("Handle: userId: " + event.source.user_id + ", reply_token: " + event.reply_token + ", message: " + event.message.text)
+    try:
+        profile = line_bot_api.get_profile(event.source.user_id)
+        print(profile.display_name)
+        print(profile.user_id)
+        print(profile.picture_url)
+        print(profile.status_message)
+    except LineBotApiError as e:
+        print('can\'t get user profile')
+
+    # return user meun
     line_bot_api.reply_message(event.reply_token, msgIdentify(event.message.text))
 
 
