7150308d3af99206784118c9fed2307db698dd96
萬俊瑋 <wei840222@gmail.com>
2018-04-20 16:37:42 +0800

    add return msg dict

--- msgProcess.py
+++ msgProcess.py
@@ -1,5 +1,11 @@
 from linebot.models import *
 
+aboutMe = '''
+我的名字叫 萬俊瑋
+性別是男生
+興趣是聽音樂、跳舞、看Youtube
+'''
+
 def getTemplte():
     message = TemplateSendMessage(
         alt_text='Buttons template',
@@ -27,7 +33,9 @@ def getTemplte():
     return message
 
 def msgIdentify(msg):
-    if msg == '樣板':
-        return getTemplte()
-    if msg == '你好':
-        return TextSendMessage(text='你好')
+    msgDict = {'@關於我':aboutMe}
+    reMsg = msgDict[msg]
+    if reMsg is not None:
+        return TextSendMessage(text=reMsg)
+    else:
+        return TextSendMessage(text='你說什麼？')
