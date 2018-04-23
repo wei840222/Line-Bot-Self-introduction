6b98c89ae27ce595582c88b7364cba764cf08e17
萬俊瑋 <wei840222@gmail.com>
2018-04-23 10:47:48 +0800

    fix time app
    'datetime.datetime' object has no attribute 'split'

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ class Time():
         self.tpeTimeZone = pytz.timezone('Asia/Taipei')
 
     def getTime(self):
-        tpeTime = self.tpeTimeZone.fromutc(datetime.utcnow())
+        tpeTime = str(self.tpeTimeZone.fromutc(datetime.utcnow()))
         date = tpeTime.split(' ')[0].split('-')
         time = tpeTime.split(' ')[1].split('.')[0].split(':')
         return TextSendMessage(text=('台北時間：' + date[0] + '年' + date[1] + '月' + date[2] + '日' + time[0] + '時' + time[1] + '分'))
